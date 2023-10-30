207c1ce2b2cc503f56d15fc81d2d23bea4877632
ParmesanHubStorage <149036914+ParmesanHubStorage@users.noreply.github.com>
2023-10-30 21:38:05 +0300

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -95,9 +95,6 @@ _G.PlayerForKill = nil
 _G.SpamChildren = false
 _G.RainbowChildName = false
 
-local Houses = {"1 [Motel]", "2 [Motel]", "3 [Motel]", "4 [Motel]", "5 [Motel]", "6 [Apartment]", "7 [Apartment]", "11 [House]", "12 [House]", "13 [House]", "14 [House]", "15 [House]", "13 [House]", "14 [House]", "15 [House]", "16 [House]", "17 [House]", "18 [House]", "19 [House]", "20 [House]", "21 [House]", "22 [House]", "23 [House]", "24 [House]", "28 [Lake Side House]", "29 [Hidden Valley House]", "30 [Lake House]", "31 [Eagle View House]", "32 [Gold Ranch House]", "33 [North Estate]", "34 [South Estate]"}
-local VehiclesList = {}
-
 local function PlayersHighlight()
 	for i,v in pairs(game.Players:GetChildren()) do
 		pcall(function()
