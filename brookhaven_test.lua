f0d371e338458d13854e3dedeccdf5a2235589da
ParmesanHubStorage <149036914+ParmesanHubStorage@users.noreply.github.com>
2023-11-03 23:59:32 +0300

    Update brookhaven_test.lua

--- brookhaven_test.lua
+++ brookhaven_test.lua
@@ -2655,4 +2655,53 @@ local Button = Tab:Button({
 	end)
 })
 
-print("9")
+Tab:Section({
+	text = "Kill Player"
+})
+
+local Button = Tab:Button({
+	name = "Kill Player",
+	callback = (function()
+		local CurrentTarget = game.Players:FindFirstChild(_G.PlayerForKill).Character
+		if not game.Players.LocalPlayer.Character:FindFirstChild("Couch") and not game.Players.LocalPlayer.Backpack:FindFirstChild("Couch") then
+			game:GetService("ReplicatedStorage").RE["1Too1l"]:InvokeServer("PickingTools", "Couch")
+		elseif game.Players.LocalPlayer.Backpack:FindFirstChild("Couch") then
+			game.Players.LocalPlayer.Backpack.Couch.Parent = game.Players.LocalPlayer.Character
+		end
+		while CurrentTarget.Humanoid.Sit == false and task.wait() do
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CurrentTarget.HumanoidRootPart.CFrame + Vector3.new(0, -5, 2)
+		end
+		game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0,-490,0)
+		wait(0.5)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+		task.wait(2)
+		if game.Players.LocalPlayer.Character:FindFirstChild("Couch") then
+			game.Players.LocalPlayer.Character.Couch.Parent = game.Players.LocalPlayer.Backpack
+		end
+	end)
+})
+
+local Dropdown = Tab:Dropdown({
+	name = "Choose a Player",
+	callback = function(Name, Value)
+		_G.PlayerForKill = Name
+	end,
+	opencallback = function()
+		Dropdown:Clear()
+		for i,v in pairs(game.Players:GetChildren()) do
+			Dropdown:Add(v.Name, nil)
+		end
+	end,
+})
+
+Tab:Section({
+	text = "Rejoin the Server"
+})
+
+local Button = Tab:Button({
+	name = "Rejoin the Server",
+	callback = (function()
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+	end)
+})
