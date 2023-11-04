1dd0e99b40a983c36aca235c32a41cb6b4b906e8
ParmesanHubStorage <149036914+ParmesanHubStorage@users.noreply.github.com>
2023-11-04 20:22:22 +0300

    Update loadstrings.lua

--- loadstrings.lua
+++ loadstrings.lua
@@ -3,7 +3,7 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 if game.PlaceId == 3200010305 or game.PlaceId == 2797881676 or game.PlaceId == 2625195454 or game.PlaceId == 3590667014 or game.PlaceId == 2951213182 or game.PlaceId == 4518266946 or game.PlaceId == 4661507759 or game.PlaceId == 4768829954 or game.PlaceId == 2215221144 or game.PlaceId == 4134003540 or game.PlaceId == 3925577908 or game.PlaceId == 4388762338 or game.PlaceId == 5071816792 or game.PlaceId == 5188855685 or game.PlaceId == 5862433299 or game.PlaceId == 7799530284 or game.PlaceId == 2500111229 or game.PlaceId == 740581508 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/CheesyLiker/bottleofwater/main/1.lua", true))()
 elseif game.PlaceId == 13772394625 or game.PlaceId == 14732610803 or game.PlaceId == 14368557094 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/XtraCheese/bottleofwater/main/blball.lua", true))()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/ParmesanHub/ScriptStorage/main/blade_ball.lua')))()
 elseif game.PlaceId == 13127800756 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/XtraCheese/bottleofwater/main/arm2.lua", true))()
 elseif game.PlaceId == 14184086618 or game.PlaceId == 14483937954 or game.PlaceId == 14666799841 or game.PlaceId == 14840620690 then
