0dcbbde5edfd2e00de5409ee55ec7249dfbcb673
ParmesanHubStorage <149036914+ParmesanHubStorage@users.noreply.github.com>
2023-11-04 12:52:45 +0300

    Create dropdown_test.lua

---
+++ dropdown_test.lua
@@ -0,0 +1,30 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/ParmesanHubStorage/Storage/main/ParmesanHub_UI_Lib.lua')))()
+
+wait(3)
+
+local Main = Library:Init({
+	name = "Brookhaven RP"
+})
+
+
+local Tab = Main:CreateTab({
+	name = "Teleport",
+	icon = "rbxassetid://15105266838"
+})
+
+Tab:Section({
+	text = "Teleport to Player"
+})
+
+local Dropdown = Tab:Dropdown({
+	name = "Teleport to Player",
+	callback = function(Name, Value)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace:FindFirstChild(Name).HumanoidRootPart.CFrame
+	end,
+	opencallback = function()
+		Dropdown:Clear()
+		for i,v in pairs(game.Players:GetChildren()) do
+			Dropdown:Add(v.Name, nil)
+		end
+	end,
+})
